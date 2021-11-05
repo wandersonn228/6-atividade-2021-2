6b9c018146df6eb5021892d2932acb8fe51d6b06
Wanderson <wandersonn228@gmail.com>
2021-11-05 12:38:51 -0300

    Atividade 6

--- src/Rotas.jsx
+++ src/Rotas.jsx
@@ -1,24 +1,31 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
 import { Route, Switch } from 'react-router'
+import Alunos from './pages/alunos/Alunos'
 import FormAlunos from './pages/alunos/FormAlunos'
+import Curso from './pages/cursos/Curso'
 import FormCursos from './pages/cursos/FormCursos'
+import Disciplina from './pages/disciplinas/Disciplina'
 import FormDisciplina from './pages/disciplinas/FormDisciplina'
+import Professores from './pages/professores/Professores'
 import FormProfessores from './pages/professores/FormProfessores'
+
 
 const Rotas = () => {
     return (
-        <>
+        <Container className="mt-3">
             <Switch>
-                <Container className="mt-3">
-                    <Route exact path="/" component={FormCursos} />
-                    <Route exact path="/cursos" component={FormCursos} />
-                    <Route exact path="/alunos" component={FormAlunos} />
-                    <Route exact path="/disciplinas" component={FormDisciplina} />
-                    <Route exact path="/professores" component={FormProfessores} />
-                </Container>
+                <Route exact path="/" component={Curso} />
+                <Route exact path="/cursos" component={Curso} />
+                <Route exact path="/cursos/create" component={FormCursos} />
+                <Route exact path="/alunos" component={Alunos} />
+                <Route exact path="/alunos/create" component={FormAlunos} />
+                <Route exact path="/disciplinas" component={Disciplina} />
+                <Route exact path="/disciplinas/create" component={FormDisciplina} />
+                <Route exact path="/professores" component={Professores} />
+                <Route exact path="/professores/create" component={FormProfessores} />
             </Switch>
-        </>
+        </Container >
     )
 }
 
